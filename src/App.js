41ec44e82b7e11645524f0e300f91b200141749a
bigliuliu <1552000425@qq.com>
2024-08-01 22:35:02 +0800

    add some contents on homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
-    <div className="bg-gray-500 w-5">
-      ReactDemos
+    <div className="font-mono flex flex-col items-center justify-center w-full">
+      <h1>Learning ReactJS start with amazing demos</h1>
+      <p>say goodbye to "Hello world ! "</p>
     </div>
   );
 }
