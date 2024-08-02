543531484351362cc5eb2a43f97177e738ae42d6
bigliuliu <1552000425@qq.com>
2024-08-02 23:09:42 +0800

    merge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 function App(){
   return (
-<<<<<<< HEAD
     <div className='relative mx-auto my-0 bg-red-400'>
       <h1 className="text-3xl font-bold underline">Learning React by amazing projects</h1>
       <p>say good bye to "Hello world"</p>
-=======
-    <div className="font-mono flex flex-col items-center justify-center w-full">
-      <h1>Learning ReactJS start with amazing demos</h1>
-      <p>say goodbye to "Hello world ! "</p>
->>>>>>> 41ec44e82b7e11645524f0e300f91b200141749a
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -34,28 +33,3 @@ module.exports = {
     port: 3000,
   },
 };
-=======
-var path = require('path');
-var HtmlWebpackPlugin =  require('html-webpack-plugin');
-
-module.exports = {
-    entry : './ReactDemos/src/index.js',
-    output : {
-        path : path.resolve(__dirname , 'dist'),
-        filename: 'index_bundle.js'
-    },
-    module : {
-        rules : [
-            {test : /\.(js)$/, use:'babel-loader'},
-            {test : /\.css$/, use:['style-loader', 'css-loader']}
-        ]
-    },
-    mode:'development',
-    plugins : [
-        new HtmlWebpackPlugin ({
-            template : 'ReactDemos/public/index.html'
-        })
-    ]
-
-}
->>>>>>> 41ec44e82b7e11645524f0e300f91b200141749a
