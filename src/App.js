f4095e64b6545271f885f4ba8f4781a3dbb59baf
bigliuliu <1552000425@qq.com>
2024-08-02 23:04:19 +0800

    add webpack and babel into project but tailwind css doesn't work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
+import React from 'react';
+function App(){
   return (
-    <div className="bg-gray-500 w-5">
-      ReactDemos
+    <div className='relative mx-auto my-0 bg-red-400'>
+      <h1 className="text-3xl font-bold underline">Learning React by amazing projects</h1>
+      <p>say good bye to "Hello world"</p>
     </div>
   );
-}
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
