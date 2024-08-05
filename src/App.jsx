7748887cdb1048f8c7d05f5b0bcfde39252b7fc5
bigliuliu <1552000425@qq.com>
2024-08-05 23:21:45 +0800

    add a code block component on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
+import { CodeBlock, dracula } from "react-code-blocks";
+// import { useState } from "react";
 function App() {
+  const code = `import { useState } from "react";
+function ReactDemos(){
+  const [des,setDes]= useState('')
+  useEffect(()=>{
+    setDes('This is my first React project')
+  },[])
+  return (<div>{des}<div>)
+}
+export default ReactDemos`;
   return (
     <>
       <div className="flex w-screen h-screen">
         <div className="w-2/3 text-center m-auto">
           <h1 className="font-bold text-7xl">Amazing React Demos</h1>
-          <p className="text-slate-500 text-2xl mt-4">Learning React by specific projects,say good bye to hello world 👋</p>
-          <p className="text-slate-500  underline cursor-pointer">start </p>
+          <p className="text-slate-500 text-2xl mt-4">
+            Learning React by{" "}
+            <span className="underline decoration-orange-600">
+              specific projects
+            </span>
+            ,say good bye to hello world 👋
+          </p>
+          <p className="text-slate-300 text-3xl mt-6 cursor-pointer after:content-['\2192']">
+            start{" "}
+          </p>
         </div>
-        <div className="w-1/3 border-white border-l-2"></div>
+        <div className="w-1/3 m-auto ml-2">
+          <div className="w-5/6 flex">
+            <CodeBlock
+              theme={dracula}
+              text={code}
+              language="javascript"
+              showLineNumbers={true}
+              customStyle={{
+                borderRadius: '5px',
+                boxShadow: '1px 2px 3px rgba(0,0,0,0.35)',
+                fontSize: '1.25rem',
+              }}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
