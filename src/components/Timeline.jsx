63a8e0af65fb0f256a722333b68c649b91f5115e
bigliuliu <1552000425@qq.com>
2024-08-07 22:43:45 +0800

    initialize timeline data

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,28 +1,57 @@
-import Timeline from '@mui/lab/Timeline';
-// import { Timeline } from '@mui/icons-material';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
-import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import HotelIcon from '@mui/icons-material/Hotel';
-import RepeatIcon from '@mui/icons-material/Repeat';
-import Typography from '@mui/material/Typography';
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+import LaptopMacIcon from "@mui/icons-material/LaptopMac";
+import HotelIcon from "@mui/icons-material/Hotel";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
 export default function CustomizedTimeline() {
-  return (
-    <Timeline position="alternate">
-      <TimelineItem>
+  const [roadMap, setRoadMap] = useState([
+    {
+      title: "🧸 animals",
+      highlight: "state",
+      description: " state have default value can be changed by click events",
+    },
+    {
+      title: "💳 staticCard",
+      highlight: "props",
+      description:
+        "use props to share datas between component and its children components",
+    },
+    {
+      title: "📖 books",
+      highlight: "context & hook",
+      description:
+        "usecontext() to share data, react hook useEffect() for components render",
+    },
+    {
+      title: "🎞pics",
+      highlight: "dynamic",
+      description: "dynamic render components",
+    },
+    {
+      title: "⚙comps",
+      highlight: "custom hooks",
+      description:
+        "custom a React Router component by useReducer and taste Tailwind CSS",
+    },
+  ]);
+  const renderTimeItem = roadMap.map((item, index) => {
+    return (
+      <TimelineItem key={index}>
         <TimelineOppositeContent
-          sx={{ m: 'auto 0' }}
+          sx={{ m: "auto 0" }}
           align="right"
           variant="body2"
-          color="text.secondary"
         >
-          9:30 am
+          {item.title}
         </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
@@ -31,65 +60,95 @@ export default function CustomizedTimeline() {
           </TimelineDot>
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
+        <TimelineContent sx={{ py: "12px", px: 2 }}>
           <Typography variant="h6" component="span">
-            Eat
+            {item.highlight}
           </Typography>
-          <Typography>Because you need strength</Typography>
+          <Typography>{item.description}</Typography>
         </TimelineContent>
       </TimelineItem>
-      <TimelineItem>
-        <TimelineOppositeContent
-          sx={{ m: 'auto 0' }}
-          variant="body2"
-          color="text.secondary"
-        >
-          10:00 am
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot color="primary">
-            <LaptopMacIcon />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
-            Code
-          </Typography>
-          <Typography>Because it&apos;s awesome!</Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot color="primary" variant="outlined">
-            <HotelIcon />
-          </TimelineDot>
-          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-        </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
-            Sleep
-          </Typography>
-          <Typography>Because you need rest</Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-          <TimelineDot color="secondary">
-            <RepeatIcon />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
-            Repeat
-          </Typography>
-          <Typography>Because this is the life you love!</Typography>
-        </TimelineContent>
-      </TimelineItem>
-    </Timeline>
+    );
+  });
+  return (
+    <>
+      {/* <Timeline position="alternate">
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            align="right"
+            variant="body2"
+            color="text.secondary"
+          >
+            9:30 am
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot>
+              <FastfoodIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography variant="h6" component="span">
+              Eat
+            </Typography>
+            <Typography>Because you need strength</Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: "auto 0" }}
+            variant="body2"
+            color="text.secondary"
+          >
+            10:00 am
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="primary">
+              <LaptopMacIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography variant="h6" component="span">
+              Code
+            </Typography>
+            <Typography>Because it&apos;s awesome!</Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="primary" variant="outlined">
+              <HotelIcon />
+            </TimelineDot>
+            <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography variant="h6" component="span">
+              Sleep
+            </Typography>
+            <Typography>Because you need rest</Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+            <TimelineDot color="secondary">
+              <RepeatIcon />
+            </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: "12px", px: 2 }}>
+            <Typography variant="h6" component="span">
+              Repeat
+            </Typography>
+            <Typography>Because this is the life you love!</Typography>
+          </TimelineContent>
+        </TimelineItem>
+      </Timeline> */}
+      <Timeline className="text-white">{renderTimeItem}</Timeline>
+    </>
   );
 }
